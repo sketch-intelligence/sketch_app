e16a989fbbdc7f69a049ec1d6efac875c7cee6d8
BeshrHmaid <BeshrHmaid@gmail.com>
2024-10-22 19:48:25 +0300

    complete clean arch structure

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,7 +1,13 @@
 import 'package:go_router/go_router.dart';
+import 'package:sketch/features/auth/presentation/views/login_view.dart';
 
 abstract class AppRouter {
   static final router = GoRouter(
-    routes: [],
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const LoginView(),
+      )
+    ],
   );
 }
